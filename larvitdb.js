00459a5018c0bb32d20b5863c55965a60a814b5e
lilleman <lilleman@larvit.se>
2015-05-20 11:24:44 +0200

    Expose the pool

--- larvitdb.js
+++ larvitdb.js
@@ -2,15 +2,14 @@
 
 var mysql = require('mysql'),
     log   = require('winston'),
-    conf,
-    pool;
+    conf;
 
 exports.setup = function(thisConf) {
-	conf = thisConf;
-	pool = mysql.createPool(conf);
+	conf         = thisConf;
+	exports.pool = mysql.createPool(conf);
 
 	// Expose getConnection()
-	exports.getConnection = pool.getConnection;
+	exports.getConnection = exports.pool.getConnection;
 
 	// Make connection test to database
 	exports.query('SELECT 1', function(err, rows) {
@@ -26,7 +25,7 @@ exports.setup = function(thisConf) {
 exports.query = function query(sql, dbFields, callback) {
 	var err;
 
-	if (pool === undefined) {
+	if (exports.pool === undefined) {
 		err = new Error('larvitdb: No pool configured. setup() must be ran with config parameters to configure a pool.');
 		log.error(err.message);
 		callback(err);
@@ -38,7 +37,7 @@ exports.query = function query(sql, dbFields, callback) {
 
 		log.debug('larvitdb: Running SQL: "' + sql + '"');
 
-		pool.query(sql, function(err, rows, rowFields) {
+		exports.pool.query(sql, function(err, rows, rowFields) {
 			// We log and handle plain database errors in a unified matter
 			if (err) {
 				err.sql = sql;
@@ -52,7 +51,7 @@ exports.query = function query(sql, dbFields, callback) {
 	} else {
 		log.debug('larvitdb: Running SQL: "' + sql + '" with dbFields = ' + JSON.stringify(dbFields));
 
-		pool.query(sql, dbFields, function(err, rows, rowFields) {
+		exports.pool.query(sql, dbFields, function(err, rows, rowFields) {
 			// We log and handle plain database errors in a unified matter
 			if (err) {
 				err.sql    = sql;
